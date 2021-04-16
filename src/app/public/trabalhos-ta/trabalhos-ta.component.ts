2fdcfc027560c1e980205f78563ff915f51c2fb7
Assistiva <82551962+Assistiva@users.noreply.github.com>
2021-04-16 08:55:43 -0300

    Add files via upload

---
+++ src/app/public/trabalhos-ta/trabalhos-ta.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'trabalhos-ta',
+  templateUrl: './trabalhos-ta.component.html',
+  styleUrls: ['./trabalhos-ta.component.scss']
+})
+export class TrabalhosTaComponent implements OnInit {
+  
+  public SORRY = '../assets/images/siteEmConstrucao.jpeg';
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
