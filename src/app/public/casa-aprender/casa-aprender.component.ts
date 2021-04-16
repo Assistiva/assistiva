73692b1bf876f6fced0379993d2357538b069d57
Assistiva <82551962+Assistiva@users.noreply.github.com>
2021-04-16 08:47:00 -0300

    Create casa-aprender.component.ts

---
+++ src/app/public/casa-aprender/casa-aprender.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'casa-aprender',
+  templateUrl: './casa-aprender.component.html',
+  styleUrls: ['./casa-aprender.component.scss']
+})
+export class CasaAprenderComponent implements OnInit {
+
+  public SORRY = '../assets/images/siteEmConstrucao.jpeg';
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
