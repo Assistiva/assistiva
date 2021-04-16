746651743123e7139ecf350585ee65d0e3d47bf1
Assistiva <82551962+Assistiva@users.noreply.github.com>
2021-04-16 08:52:49 -0300

    Add files via upload

---
+++ src/app/public/laboratorio-ta/laboratorio-ta.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'laboratorio-ta',
+  templateUrl: './laboratorio-ta.component.html',
+  styleUrls: ['./laboratorio-ta.component.scss']
+})
+export class LaboratorioTaComponent implements OnInit {
+
+  public SORRY = '../assets/images/siteEmConstrucao.jpeg';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
