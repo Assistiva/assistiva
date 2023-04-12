8560835d3f3fc812573370df0c99660ba2e09155
Kaiser Santos <kaisersantos@oimelvin.com.br>
2023-04-12 17:44:24 -0300

    Primeiras modificacoes

---
+++ assistiva/src/app/shared/services/controll-styles.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ControllStyles {
+  contraste: any = {
+    backgroundColor: "white",
+    color: "black",
+    "text-align": "justify",
+    "font-family": "Arial",
+    "line-height": "normal",
+  };
+  fontSize: number = 16;
+  alignText: string = "justify";
+  fontFamily: string = "Arial";
+  lineHeight: string = "normal";
+  imagesVisibility: any = { visibility: "visible" };
+
+  constructor() {}
+
+  public getContrast() {
+    return this.contraste;
+  }
+
+  public getFontSize() {
+    return this.fontSize;
+  }
+
+  public getAlign() {
+    // start, end e center
+    return this.alignText;
+  }
+
+  public getFontFamily() {
+    return this.fontFamily;
+  }
+
+  public getImagesVisibility() {
+    return this.imagesVisibility;
+  }
+
+  public setContraste(contraste: any) {
+    this.contraste = contraste;
+  }
+
+  public setFontSize(font: number) {
+    this.fontSize = font;
+  }
+
+  public setAlign(align: string) {
+    this.alignText = align;
+  }
+
+  public setFontFamily(font: string) {
+    this.fontFamily = font;
+  }
+
+  public setLineHeight(lineHeight: string) {
+    this.lineHeight = lineHeight;
+  }
+
+  public setImagesVisibility(img: any) {
+    this.imagesVisibility = img;
+  }
+}
